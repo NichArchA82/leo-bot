1240953936cd7181dfac238609a1231ddbe63e62
NichArchA82 <64152648+NichArchA82@users.noreply.github.com>
2024-06-15 21:28:02 +0000

    feat: add https to server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -6,6 +8,13 @@ app.get('/', (req, res) => {
     res.sendStatus(200);
 })
 
-app.listen(port, () => {
-    console.log(`The server is listening on port ${port}`)
-})
+// Path to your SSL certificate and key
+const sslOptions = {
+    key: fs.readFileSync(process.env.SERVER_KEY),
+    cert: fs.readFileSync(process.env.SERVER_CERT)
+  };
+  
+  // Create HTTPS server
+  https.createServer(sslOptions, app).listen(port, () => {
+    console.log(`HTTPS Server running on port ${port}`);
+  });
