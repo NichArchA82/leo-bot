9fb59a816fb8fa9bd27f9225f9bf4ea365b8c0bc
NichArchA82 <64152648+NichArchA82@users.noreply.github.com>
2024-06-25 20:22:57 +0000

    feat: add invite command

---
+++ command-handler/src/commands/invite.js
@@ -0,0 +1,47 @@
+import { ApplicationCommandOptionType, PermissionFlagsBits } from 'discord.js';
+import commandTypes from '../cmd-handler/command-types.js';
+
+export default {
+    description: 'Creates a One time invite that expires in a week',
+    type: commandTypes.Slash,
+    devCmd: false,
+    guildOnly: true,
+    permissions: [PermissionFlagsBits.Administrator],
+    options: [
+        {
+            name: 'sponsor',
+            description: 'The sponsor to dm the link to',
+            type: ApplicationCommandOptionType.User,
+            required: false,
+        },
+    ],
+
+    run: async ({ interaction, response, args, guild }) => {
+        const [sponsor] = (args && args.length > 0) ? args : ['None'];
+        const maxAge = 7 * 24 * 60 * 60; // 1 week
+        const invite = await interaction.guild.invites.create(interaction.channel.id, {
+        maxAge: maxAge,
+        maxUses: 1,
+        unique: true,
+        });
+        if (sponsor === 'None') {
+            response({
+                content: `${invite.url}`,
+                ephemeral: true,
+            });
+        } else {
+            const member = await guild.members.fetch(sponsor);
+            await member.send({
+                content: `${invite.url}`
+            });
+            response({
+                content: `${member} was sent invite: ${invite.url}`,
+                allowedMentions: {
+                    roles: [],
+                    users: []
+                },
+                ephemeral: true,
+            });
+        }
+    }
+}
