0e273e0ed9543fe1d51c87c28fffe220b29b5d63
NichArchA82 <64152648+NichArchA82@users.noreply.github.com>
2024-07-23 22:17:24 +0000

    fix: function name

--- bot/src/features/eval.js
+++ bot/src/features/eval.js
@@ -1,11 +1,11 @@
 import cron from 'node-cron';
-import eval from '../tasks/eval.js';
+import evalTask from '../tasks/eval.js';
 
 export default (client, handler) => {
     // Scheduled every Monday at 11pm
     // 0 23 * * 1
     cron.schedule('* * * * *', async () => {
-       eval(client, handler); 
+       evalTask(client, handler); 
     }, {
         scheduled: true,
         timezone: "America/Denver"
