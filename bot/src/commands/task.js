efc613b0b3520e3912f39c941daa03c512c1ad29
NichArchA82 <64152648+NichArchA82@users.noreply.github.com>
2024-07-23 23:02:54 +0000

    feat: add task command

---
+++ bot/src/commands/task.js
@@ -0,0 +1,50 @@
+import CommandTypes from 'command-handler/src/cmd-handler/command-types.js';
+import { PermissionFlagsBits } from 'discord.js';
+import { recruit, evalTask } from '../tasks';
+
+export default {
+    description: 'Run scheduled tasks on command',
+    type: CommandTypes.Slash,
+    guildOnly: true,
+    permissions: [PermissionFlagsBits.Administrator],
+    options: [
+        {
+            name: 'eval',
+            description: 'run the eval task',
+            type: ApplicationCommandOptionType.Subcommand,
+        },
+        {
+            name: 'recruit',
+            description: 'run the recruit task',
+            type: ApplicationCommandOptionType.Subcommand,
+        }
+    ],
+
+    run: async ({ response, handler }) => {
+        const subCommand = interaction.options.getSubcommand(false);
+
+        if (!handler.isDbConnected) {
+            response({
+                content: 'db error: No Connection. Contact developers for help',
+                ephemeral: true,
+            })
+
+            return;
+        }
+
+        if (subCommand === 'eval') {
+            evalTask(handler.client, handler);
+            response({
+                content: 'eval task executed',
+                ephemeral: true,
+            });
+            
+        } else if (subCommand === 'recruit') {
+            recruit(handler.client, handler);
+            response({
+                content: `recruit task executed`,
+                ephemeral: true
+            });
+        }
+    }
+}
